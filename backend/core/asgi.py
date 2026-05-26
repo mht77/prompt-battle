6be34b8406536e1b99645c7ff9858dfcf88859e6
Mohammad Toosi <m.h.t.1377@gmail.com>
2026-05-26 15:31:31 +0200

    Initialize Django before importing ASGI routes

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,12 +1,16 @@
 import os
 import dotenv
+import django
+
+dotenv.load_dotenv()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import game.routing
 
-dotenv.load_dotenv()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
